20d59f6a0dfaec54812417afa693ed4d8e30535d
Nick Battjes <nhbattjes@gmail.com>
2018-11-15 10:43:41 -0800

    add analytics

--- gameFiles/breakStijl.js
+++ gameFiles/breakStijl.js
@@ -1,7 +1,6 @@
 import Game from './game';
 
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("hi");
   const canvasEl = document.getElementById("myCanvas");
   canvasEl.width = Game.canvasWidth;
   canvasEl.height = Game.canvasHeight;
